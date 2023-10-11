63e6b0f1f3eb946a0fa361ca435f164f1745fbca
Hritik Pawar <hritikpawar525@.com>
2023-10-11 14:35:04 +0530

    currency changer

--- 06currencyconverter/src/hooks/useCurrencyInfo.js
+++ 06currencyconverter/src/hooks/useCurrencyInfo.js
@@ -5,11 +5,14 @@ function useCurrencyInfo(currency) {
     useEffect(()=>{
         fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
         .then((res)=>{
-            return (JSON.parse(res))
+            return (res.json())
         })
         .then((res)=>{
-            setData(res.currency)
+            setData(res[currency])
         })
         console.log(data);
-    },[])
+    },[currency])
+    return data
 }
+
+export default useCurrencyInfo
